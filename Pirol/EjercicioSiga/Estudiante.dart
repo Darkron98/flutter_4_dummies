de793708a24e1aead4650fc3951a8c18766f1892
Pixulegos <felipe.910@hotmail.com>
2023-04-04 18:42:45 -0500

    04042023_0642
    No funciona xd

--- Pirol/EjercicioSiga/Estudiante.dart
+++ Pirol/EjercicioSiga/Estudiante.dart
@@ -1,4 +1,5 @@
 import 'Util.dart';
+import 'dart:io';
 
 class Estudiante{
   final int codigo;
@@ -12,59 +13,60 @@ class Estudiante{
     this.codPlan = 0,
     this.nombre = '',
     this.apellido = '',
-    this.notas = [];
-  })
+    notas,
+  }):notas = List.filled(4, 0); // Constructor DEBE terminar con ;
 
-  set codigo(int codigo){
-    this.codigo = codigo;
+  set codigo(int? codigo){
+    if(codigo == null){
+      this.codigo = Util().leerInt("Digite el codigo:\n");
+    }
+    else{
+      this.codigo = codigo;
+    }
   }
 
-  set codigo(){
-    this.codigo = Util().leerInt("Digite el codigo: ");
+  set codPlan(int? codPlan){
+    if(codPlan == null){
+      this.codPlan = Util().leerInt("Digite el codigo de la materia:\n");
+    }
+    else{
+      this.codPlan = codPlan;
+    }
   }
 
-  set codPlan(int codPlan){
-    this.codPlan = codPlan;
+  set nombre(String? nombre){
+    if(nombre == null){
+      this.nombre = stdin.readLineSync(); 
+    }
+    else{
+      this.nombre = nombre;
+    }
   }
 
-  set nombre(String nombre){
-    this.nombre = nombre;
+  set apellido(String? apellido){
+    if(apellido == null){
+      this.apellido = stdin.readLineSync();
+    }  
+      this.apellido = apellido;
   }
 
-  set apellido(String apellido){
-    this.apellido = apellido;
-  }
-
-  set notas(List notas){
-    this.notas = notas;
-  }
-
-  set notas(){
-    for( int i = 0; i < 3; i++){
+  set notas(List? notas){
+    if(notas == null){
+      for( int i = 0; i < 3; i++){
       this.notas[i] = Util().leerDouble("Digite la nota ${i+1}");
     }
     this.notas[3] = this.notas[0] * 0.3 + this.notas[1] * 0.3 + this.notas[2] * 0.4;
+    }
+    else{
+      this.notas = notas;
+    }
   }
 
-  int get gcodigo{
-    return this.codigo;
+  @override
+  String toString() {
+    return '''Estudiante: ${this.nombre} ${this.apellido}
+Codigo: ${this.codigo}
+Materia: ${this.codPlan}
+Notas: ${notas}''';
   }
-
-  int get gcodPlan{
-    return this.codPlan;
-  }
-
-  String get gnombre{
-    return this.nombre;
-  }
-
-  String get gapellido{
-    return this.apellido;
-  }
-
-  double gnotas(int index){
-    return this.notas[index];
-  }
-
-  
 }
--- Pirol/EjercicioSiga/Siga.dart
+++ Pirol/EjercicioSiga/Siga.dart
@@ -1,7 +1,81 @@
-main(){
+import 'dart:math';
 
+import 'Estudiante.dart';
+import 'Util.dart';
+
+main(){
+  Siga e = new Siga();
+  e.menu();
 }
 
 class Siga{
+  List<Estudiante> arreglo = [];
 
+  void leerCant(){
+    int tam = Util().leerInt("Ingrese cantidad de estudiantez:\n");
+    arreglo = new List.filled(tam, Estudiante());
+  }
+
+  void leerInfo(){
+    for(int i = 0; i < arreglo.length; i++){
+      arreglo[i].codigo;
+      arreglo[i].codPlan;
+      arreglo[i].nombre;
+      arreglo[i].apellido;
+      arreglo[i].notas;
+    }
+  }
+
+  void verListado(){
+    for(int i = 0; i < arreglo.length; i++){
+      print('${arreglo[i]} \n');
+    }
+  }
+
+  void consultarCodigo(){
+    bool existe = false;
+    int codBuscar = Util().leerInt("Ingrese codigo a buscar:\n");
+    for(int i = 0; i < arreglo.length; i++){
+      if(codBuscar == arreglo[i].codigo){
+        existe = true;
+        print(e);
+        break;
+      }
+    }
+    if(!existe){
+      print("Codigo estudiantil inexistente.\n");
+    }
+  }
+
+  void aprobados(){
+    for(int i = 0; i < arreglo.length; i++){
+      if(arreglo[i].notas[3] >= 2.96){
+        print('${arreglo[i].codigo}: Aprobado\n');
+      }
+      else if(arreglo[i].notas[3] > 2.45 && arreglo[i].notas[3] < 2.96){
+        print('${arreglo[i].codigo}: Habilitable\n');
+      }
+      else{
+        print('${arreglo[i].codigo}: Reprobado\n');
+      }
+    }
+  }
+
+  void calcularPromedio(){
+    double promedio = 0;
+    for(int i = 0; i < arreglo.length; i++){
+      promedio+= arreglo[i].notas[3];
+    }
+    promedio/= arreglo.length;
+    print("El promedio de definitivas de los ${arreglo.length} estudiantes fue: $promedio\n");
+  }
+
+  void menu(){
+    leerCant();
+    leerInfo();
+    verListado();
+    consultarCodigo();
+    aprobados();
+    calcularPromedio();
+  }
 }
